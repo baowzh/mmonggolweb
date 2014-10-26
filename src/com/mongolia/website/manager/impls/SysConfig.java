7a0df9fe28d8d45977ce384e1821ffd0402cfeeb
baowzh <imubwz@126.com>
2014-10-26 14:57:02 +0800

    新版界面

--- src/com/mongolia/website/manager/impls/SysConfig.java
+++ src/com/mongolia/website/manager/impls/SysConfig.java
@@ -17,6 +17,16 @@ public class SysConfig {
 	private Integer topdocumentcount;
 	@Value("${selecteddoccount}")
 	private Integer selecteddoccount;
+	@Value("${newusercount}")
+	private Integer newusercount;
+	@Value("${topusercount}")
+	private Integer topusercount;
+	@Value("${hotdoccount}")
+	private Integer hotdoccount;
+	@Value("${recentdoccount}")
+	private Integer recentdoccount;
+	@Value("${activeusercount}")
+	private Integer activeusercount;
 
 	public String getSitename() {
 		return sitename;
@@ -57,5 +67,46 @@ public class SysConfig {
 	public void setSelecteddoccount(Integer selecteddoccount) {
 		this.selecteddoccount = selecteddoccount;
 	}
+
+	public Integer getNewusercount() {
+		return newusercount;
+	}
+
+	public void setNewusercount(Integer newusercount) {
+		this.newusercount = newusercount;
+	}
+
+	public Integer getTopusercount() {
+		return topusercount;
+	}
+
+	public void setTopusercount(Integer topusercount) {
+		this.topusercount = topusercount;
+	}
+
+	public Integer getHotdoccount() {
+		return hotdoccount;
+	}
+
+	public void setHotdoccount(Integer hotdoccount) {
+		this.hotdoccount = hotdoccount;
+	}
+
+	public Integer getRecentdoccount() {
+		return recentdoccount;
+	}
+
+	public void setRecentdoccount(Integer recentdoccount) {
+		this.recentdoccount = recentdoccount;
+	}
+
+	public Integer getActiveusercount() {
+		return activeusercount;
+	}
+
+	public void setActiveusercount(Integer activeusercount) {
+		this.activeusercount = activeusercount;
+	}
+	
 
 }
